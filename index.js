fbcfad71fa82a488e7ec86ba4791ea7fa6397348
Yoda <fathermxcker@gmail.com>
2020-06-06 17:02:52 +0800

    fix modal synax at HTML and specific dataset-id at JS

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 
   const dataPanel = document.querySelector('#data-panel')
 
+
   axios.get(INDEX_URL)
     .then((response) => {
       data.push(...response.data.results) //展開運算子...把陣列元素展開
@@ -14,7 +15,6 @@
       displayDataList(data)
     })
     .catch((err) => console.log(err))
-
 
 
   function displayDataList(data) {
@@ -29,52 +29,24 @@
             <div class="card-body">
               <h5 class="card-title">${item.title}</h5>
             </div>
-
-            <!-- Button trigger modal -->
+            <!-- Button trigger modal  dataset solution-->
             <div class="card-footer">
-              <button type="button" class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal">
+              <button type="button" class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">
                 more
               </button>
-
-
-              <!-- Modal -->
-              <div class="modal fade" id="show-movie-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                <div class="modal-dialog modal-lg" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="show-movie-title">Modal title</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">x</span>
-                      </button>
-                    </div>
-                    <div class="modal-body" id="show-movie-body">
-
-                      <div class="row">
-                        <div class="col-sm-8" id="show-movie-image">
-                        </div>
-                        <div class="col-sm-4">
-                          <p><em id="show-movie-date"></em></p>
-                          <p id="show-movie-description"></p>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
             </div>
-
-
-
           </div>
         </div>
       `
       dataPanel.innerHTML = htmlContent
     })
+
+
+    dataPanel.addEventListener('click', (event) => {
+      if (event.target.matches('.btn-show-movie')) {
+        //catch specific dataset id at line34
+        console.log(event.target.dataset.id)
+      }
+    })
   }
-
-
 })()
