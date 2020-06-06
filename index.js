657264bf725a5829fe9f0319dd90bcf31582f7c5
Yoda <fathermxcker@gmail.com>
2020-06-06 11:34:15 +0800

    add note index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
   const BASE_URL = 'https://movie-list.alphacamp.io'
   const INDEX_URL = BASE_URL + '/api/v1/movies/'
   const POSTER_URL = BASE_URL + '/posters/'
-  const data = []
+  const data = [] // 因為 data 不應該是會變動的固定資料，所以用 const
 
 
   axios.get(INDEX_URL)
     .then((response) => {
-      data.push(...response.data.results)
-      console.log(data)
+      data.push(...response.data.results) //展開運算子...把陣列元素展開
+      // console.log(data)
 
     })
     .catch((err) => console.log(err))
