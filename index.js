6a1c16fd89a503375dca0c266e1cd7592824fe3b
Yoda <fathermxcker@gmail.com>
2020-06-06 11:12:03 +0800

    modify index.js at dev

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 (function () {
   // write your code here
+  const BASE_URL = 'https://movie-list.alphacamp.io'
+  const INDEX_URL = BASE_URL + '/api/v1/movies/'
+  const POSTER_URL = BASE_URL + '/posters/'
+  const data = []
+
+
+  axios.get(INDEX_URL)
+    .then((response) => {
+
+    })
+    .catch()
 })()
